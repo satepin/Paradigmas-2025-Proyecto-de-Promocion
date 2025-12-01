ea399bc170662e43032e30a44574cd44887aec50
Satepin <salva2rterranova@gmail.com>
2025-11-30 22:27:03 -0300

    logicjs removido

--- src/core/tools/modulos/logicjs.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'logicjs' {
-  export function lvar(name?: string): any;
-  export function eq(a: any, b: any): any;
-  export function and(...goals: any[]): any;
-  export function or(...goals: any[]): any;
-  export function run(n: number | '*', ...args: any[]): any;
-}
